5371a30fbaa43765f0a581b0c753896201ff8a0f
Kadeejath Salaha <codestudy475@gmail.com>
2024-05-10 12:40:01 +0530

    Router exchange

--- olx/src/App.js
+++ olx/src/App.js
@@ -2,22 +2,22 @@ import { useState } from "react";
 import "./App.css";
 import About from "./container/About";
 import Profile from "./container/Profile";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 function App() {
   const [state, setState] = useState("");
 
-  let component;
-  if (state === "about") {
-    component = <About></About>;
-  }
-  if (state === "profile") {
-    component = <Profile></Profile>;
-  }
   return (
     <div className="App">
-      <button onClick={() => setState("about")}>About</button>
-      <button onClick={() => setState("profile")}>Profile</button>
-      {component}
+      <BrowserRouter>
+        <Link to="/about">About page</Link>
+        <br></br>
+        <Link to="/profile">Profile page</Link>
+        <Routes>
+          <Route Component={About} path="/about"></Route>
+          <Route Component={Profile} path="/profile"></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
