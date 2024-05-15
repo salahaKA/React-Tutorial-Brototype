c7732d30f975261e73c50129e634a550e7419a75
Kadeejath Salaha <codestudy475@gmail.com>
2024-05-15 21:58:28 +0530

    Signup fetch user data in console

--- olx_clone/src/App.js
+++ olx_clone/src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
+import Signup from "./Pages/Signup";
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home></Home>}></Route>
+          <Route path="/signup" element={<Signup></Signup>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- olx_clone/src/Components/Signup/Signup.js
+++ olx_clone/src/Components/Signup/Signup.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Logo from "../../olx-logo.png";
 import "./Signup.css";
 function Signup() {
+  const [Username, setUsername] = useState("");
+  const [Email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(Username);
+  };
+
   return (
     <div>
       <div className="signupParentDiv">
         <img width="200px" height="200px" src={Logo}></img>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="fname">Username</label>
           <br />
           <input
             className="input"
             type="text"
+            value={Username}
+            onChange={(e) => setUsername(e.target.value)}
             id="fname"
             name="name"
             defaultValue="John"
@@ -23,6 +35,8 @@ function Signup() {
           <input
             className="input"
             type="email"
+            value={Email}
+            onChange={(e) => setEmail(e.target.value)}
             id="fname"
             name="email"
             defaultValue="John"
@@ -33,6 +47,8 @@ function Signup() {
           <input
             className="input"
             type="number"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
             id="lname"
             name="phone"
             defaultValue="Doe"
@@ -43,13 +59,15 @@ function Signup() {
           <input
             className="input"
             type="password"
+            value={Password}
+            onChange={(e) => setPassword(e.target.value)}
             id="lname"
             name="password"
             defaultValue="Doe"
           />
           <br />
           <br />
-          <button>Signup</button>
+          <button type="submit">Signup</button>
         </form>
         <a>Login</a>
       </div>
