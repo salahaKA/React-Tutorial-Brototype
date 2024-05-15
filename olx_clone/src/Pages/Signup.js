3df3eb63c85ac243d3198526955986fecfd0d333
Kadeejath Salaha <codestudy475@gmail.com>
2024-05-15 17:12:43 +0530

    SignUp,login,Create and View frontend All set

---
+++ olx_clone/src/Pages/Signup.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Signup from "../Components/Signup/Signup";
+
+function SignupPage() {
+  return (
+    <div>
+      <Signup></Signup>
+    </div>
+  );
+}
+
+export default SignupPage;
