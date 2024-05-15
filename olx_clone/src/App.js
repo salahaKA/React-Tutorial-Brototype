e34508c950c63bb3073424d347fcfd49a7aca4e4
Kadeejath Salaha <codestudy475@gmail.com>
2024-05-15 16:47:22 +0530

    done header and footer

--- olx_clone/src/App.js
+++ olx_clone/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 
+import Home from "./Pages/Home";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Home></Home>
     </div>
   );
 }
